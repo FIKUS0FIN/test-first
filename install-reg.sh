e953f5e90edf96cc4843d215284ef633683a9eb0
FIKUS0FIN <dmytro.symonenko.ui.2014@lpnu.ua>
2017-06-01 20:07:04 +0300

    content

---
+++ install-reg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+	# instaling postgreSQL 
+#sudo apt-get update
+#sudo apt-get install -y postgresql postgresql-contrib
+#	#adding user to Linux vm
+#sudo useradd -m -p 22446688 -s /bin/bash user
+
+# install redis 
+
+sudo apt-get update
+sudo apt-get install -y  build-essential tcl
+cd /tmp
+curl -O http://download.redis.io/redis-stable.tar.gz
+tar xzvf redis-stable.tar.gz
+cd redis-stable
+make
+sudo make install
+
+
+
+#configure db and user pri...
+##      sudo su - postgres  
+##      createuser user
+##      createdb -O user database  
+##             exit
+
+	#test user and access    for Test 
+	#sudo su - user 
+	#psql -d databese -U user
+	#\q
+	#exit
+#done part
+
+
+
