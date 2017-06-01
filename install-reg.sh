9d6c013b71125594640dc106ef77159ffcb0bd96
FIKUS0FIN <dmytro.symonenko.ui.2014@lpnu.ua>
2017-06-01 20:18:19 +0300

    fil

--- install-reg.sh
+++ install-reg.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 	# instaling postgreSQL 
-#sudo apt-get update
-#sudo apt-get install -y postgresql postgresql-contrib
-#	#adding user to Linux vm
-#sudo useradd -m -p 22446688 -s /bin/bash user
+
+sudo apt-get update
+sudo apt-get install -y postgresql postgresql-contrib
+
+	#adding user to Linux vm
+
+sudo useradd -m -p 22446688 -s /bin/bash user
+
+# install nginx with ssl 
+
+cd nginx-ssl/
+chmod +x bootstrap.sh 
+./bootstrap.sh 
+
 
 # install redis 
 
@@ -15,7 +25,6 @@ tar xzvf redis-stable.tar.gz
 cd redis-stable
 make
 sudo make install
-
 
 
 #configure db and user pri...
